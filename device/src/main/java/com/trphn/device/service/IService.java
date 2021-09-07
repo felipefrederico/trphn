b4c41fc2cfe510c0718c5f667d238fb2ae782189
Felipe Frederico <felipe.frederico@gmail.com>
2021-09-07 20:52:59 +0100

    Unit Tests - Device Service Implementation

--- device/src/main/java/com/trphn/device/service/IService.java
+++ device/src/main/java/com/trphn/device/service/IService.java
@@ -1,14 +1,12 @@
 package com.trphn.device.service;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.UUID;
 
-public interface Service<T> { 
+public interface IService<T> { 
     T save(T t);
     T findById(UUID id);
     Collection<T> findAll();
-    T patch(UUID id, Map<Object, Object> fields);
     void deleteById(UUID id);
     Collection<T> filter(String brand, String name);
     T update(T t);
---
+++ device/src/main/java/com/trphn/device/service/impl/DeviceService.java
@@ -0,0 +1,57 @@
+package com.trphn.device.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import com.trphn.device.dao.Dao;
+import com.trphn.device.model.Device;
+import com.trphn.device.service.IService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class DeviceService implements IService<Device>{
+    private final Dao<Device> deviceDao;
+
+    @Override
+    public Device save(Device device) {
+        log.info("DeviceService - save {}", device.toString());
+        return deviceDao.save(device);
+    }
+
+    @Override
+    public Device findById(UUID id) {
+        log.info("DeviceService - findById {}", id.toString());
+        return deviceDao.findById(id);
+    }
+
+    @Override
+    public Collection<Device> findAll() {
+        log.info("DeviceService - findAll");
+        return deviceDao.findAll();
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        log.info("DeviceService - deleteById {}", id.toString());
+        deviceDao.delete(id);
+    }
+
+    @Override
+    public Collection<Device> filter(String brand, String name) {
+        log.info("DeviceService - search");
+        return deviceDao.filter(brand, name);
+    }
+
+    @Override
+    public Device update(Device device) {
+        log.info("DeviceService - deleteById {}", device.toString());
+        return deviceDao.update(device);
+    }
+
+}
